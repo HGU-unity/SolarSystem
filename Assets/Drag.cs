a3798cecc65e1d01b07d7129eb4c17b813abe796
Geoyeob Kim <gasbank@gmail.com>
2018-07-15 23:01:33 +0900

    Solar System initial push

---
+++ Assets/Drag.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    RectTransform rect;
+    Vector3 beginDragWorldPoint;
+    Vector3 rectPositionAtBeginDrag;
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnBeginDrag");
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rect, eventData.position, null, out beginDragWorldPoint);
+        rectPositionAtBeginDrag = rect.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+        Vector3 dragWorldPoint;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rect, eventData.position, null, out dragWorldPoint);
+        rect.position = rectPositionAtBeginDrag + (dragWorldPoint - beginDragWorldPoint);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnEndDrag");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        eventData.useDragThreshold = false;
+    }
+}
---
+++ Assets/Revolve.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Revolve : MonoBehaviour
+{
+    public float rate = 100.0f;
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * rate);
+    }
+}
